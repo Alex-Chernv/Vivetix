0bcb9af0d7210d139ae89c3aeecad66c1b2e0cdf
Alexandr Chernov <alexandr_chernov@square-nest.com>
2020-07-20 17:18:51 +0300

    Feature/This_is_first_commit_added_logic

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Vivetix.
+        </p>
+        <a
+          className="App-link"
+          href="https://vivetix.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Original Site
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
